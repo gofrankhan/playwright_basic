c9b8f4fc18e161b45e99bf6bfdac2ad1496ddcfc
Md. Gofran Khan <gofran0123@gmail.com>
2023-03-09 13:45:17 +0600

    funding account with generate invoice per custom field

--- tests/fn_common/accountant/funding_account.spec.js
+++ tests/fn_common/accountant/funding_account.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test("Accountant", async ({ page }) => {
+test("Accountant Finance Account Creation", async ({ page }) => {
     await page.goto("https://ui-qa3.fndev.net");
     await page.getByRole('button', { name: "Continue", exact: true }).click();
     await page.locator("#username").fill("accountant");
@@ -9,7 +9,15 @@ test("Accountant", async ({ page }) => {
     await page.getByRole('button', { name: 'Funding' }).click();
     await page.getByRole('link', { name: 'Funding Options' }).click();
     await page.getByText('Add', { exact: true }).click();
-    await page.getByRole('combobox', { name: 'Company Name:' }).selectOption('9739');
-    await page.locator(`xpath=//input[@id='typeahead_input']`).fill('sdfgsdfg');
+    await page.getByRole('combobox', { name: 'Company Name:' }).selectOption('25954');
+    await page.waitForTimeout(2000);
+    await page.locator(`xpath=//input[@id='typeahead_input']`).type('Buyer Field 01', { delay: 100 });
+    await page.locator(`xpath=//strong[@class='tt-highlight']`).click();
+    await page.waitForTimeout(2000);
+    const isVisible1 = await page.locator(`#clearBtn`).isVisible();
+    console.log(`Element is visible: ${isVisible1}`);
+    await page.locator(`#clearBtn`).click();
+    const isVisible = await page.locator(`#clearBtn`).isVisible(); `:q`
+    console.log(`Element is visible: ${isVisible}`);
     await page.getByRole('button', { name: "Update", exact: true }).click();
 });
