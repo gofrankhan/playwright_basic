91e2774141905766b726483cab3aedc966c75887
Md. Gofran Khan <gofran0123@gmail.com>
2023-03-12 00:26:41 +0600

    network setup full

--- tests/fn_common/staff_user/network_setup.spec.js
+++ tests/fn_common/staff_user/network_setup.spec.js
@@ -11,9 +11,14 @@ test('Network Setup', async ({ page }) => {
     await page.locator(`xpath=//input[@type='search']`).fill("Field Nation Staffing");
     await page.locator(`xpath=//div[@class='js-fixed-data-table-cell-row-0']//a[text()='Field Nation Staffing']`).click();
     await page.locator(`xpath=//span[@class='TitleBar__icon__Afy5r TitleBar__icon--settings__331QS']`).click();
+    await page.locator(`xpath=//button/div/div[text()='Payment, Fees, and Access']`).click();
+    await page.locator(`xpath=//span[@class='icon-plus']`).click();
+    await page.locator(`//input[@class='resizable-input']`).fill("Test B00016");
+    await page.keyboard.press('Enter');
+    await page.locator(`xpath=//button[@data-testid='Add']`).click();
     await page.waitForTimeout(2000);
-    //var settingLocator = await page.locator(`xpath=//button[@data-testid='[object Object]']`);
-    //await page.click(settingLocator[1]);
+    await page.locator(`xpath=//button[@data-testid='Apply Changes']`).click();
+    await page.waitForTimeout(2000);
 });
 
 //npx playwright test fn_common/staff_user/network_setup.spec.js --headed --project=chromium
