7075513bcab89b2a122ecc8cf8b4f1082c42657b
Md. Gofran Khan <gofran0123@gmail.com>
2023-04-20 20:46:25 +0600

    Create a work order

--- tests/support-cases/work_order_cases.spec.js
+++ tests/support-cases/work_order_cases.spec.js
@@ -11,28 +11,28 @@ test('Create Work Order Case - How To/Technical assitance', async ({ page }) => 
     // Create a Work Order
     await page.locator('#NavigationV2').getByTestId('IconButton').first().click();
     await page.getByRole('link', { name: 'Work Order' }).click();
-    //await page.getByTestId("advancedWorkOrderTitle").fill('Work Order Title');
     await page.getByTestId('typeOfWorkId').getByText('Select ...').click();
     await page.getByRole('option', { name: 'Automated Teller Machine (ATM)' }).click();
     await page.waitForTimeout(2000);
     await page.getByTestId('serviceTypeSelection').getByText('Please choose one or more service type').click();
     await page.getByRole('option', { name: 'Installation', exact: true }).click();
     await page.waitForTimeout(2000);
-    //await page.locator('#WorkOrderFormContainer').getByTestId('Loader-wrapper').click();
     await page.locator(`xpath=//div[text()='Use saved location']`).click();
     await page.getByTestId('locationId').getByText('Select ...').click();
-    //await page.getByTestId('locationId').fill("M");
     await page.keyboard.press('M');
     await page.getByRole('option', { name: 'MyAddress' }).click();
     await page.getByTestId('Schedule').click();
     await page.getByRole('link', { name: 'Tomorrow 8am' }).click();
-    await page.getByTestId('js-toolbar1').fill("Service Description");
-    await page.getByTestId('js-toolbar2').fill("Confidential Information");
+    await page.locator(`xpath=//div[@class="ql-editor ql-blank"]`).first().fill("Service Description");
+    await page.locator(`xpath=//div[@class="ql-editor ql-blank"]`).nth(-1).fill("Confidential Information");
+    await page.locator(`xpath=//div[text()='Fixed']`).click();
     await page.getByTestId('baseAmountInput').fill("200");
     await page.getByTestId('approxHoursFixedInput').fill("2");
-    await page.locator(`xpath=//div[text()='Fixed']`).click();
-    await page.locator(`xpath=//div[@class='Select-value']`).fill("Default");
-    await page.keyboard.press('Enter');
+    await page.getByTestId("advancedWorkOrderTitle").fill('Work Order Title');
+    //await page.locator('#financeId').click();
+    //await page.getByRole('option', { name: '/Default/' }).click();
+    //await page.keyboard.press('Enter');
+    await page.getByRole('button', { name: "Publish", exact: true }).click();
 
 
     //Naviage to support case page
