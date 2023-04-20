9b5ce8b328ba0a1474425c646ebd4837c4c89717
Md. Gofran Khan <gofran0123@gmail.com>
2023-04-21 01:40:09 +0600

    create work order report problem -p artially

--- tests/support-cases/work_order_cases.spec.js
+++ tests/support-cases/work_order_cases.spec.js
@@ -10,7 +10,7 @@ test('Create Work Order Case - How To/Technical assitance', async ({ page }) => 
 
     // Create a Work Order
     await page.locator('#NavigationV2').getByTestId('IconButton').first().click();
-    await page.getByRole('link', { name: 'Work Order' }).click();
+    await page.getByRole('link', { name: 'Work Order', exact: true }).first().click();
     await page.getByTestId('typeOfWorkId').getByText('Select ...').click();
     await page.getByRole('option', { name: 'Automated Teller Machine (ATM)' }).click();
     await page.waitForTimeout(2000);
@@ -21,18 +21,30 @@ test('Create Work Order Case - How To/Technical assitance', async ({ page }) => 
     await page.getByTestId('locationId').getByText('Select ...').click();
     await page.keyboard.press('M');
     await page.getByRole('option', { name: 'MyAddress' }).click();
+    await page.waitForTimeout(2000);
     await page.getByTestId('Schedule').click();
     await page.getByRole('link', { name: 'Tomorrow 8am' }).click();
+    await page.waitForTimeout(2000);
     await page.locator(`xpath=//div[@class="ql-editor ql-blank"]`).first().fill("Service Description");
     await page.locator(`xpath=//div[@class="ql-editor ql-blank"]`).nth(-1).fill("Confidential Information");
     await page.locator(`xpath=//div[text()='Fixed']`).click();
     await page.getByTestId('baseAmountInput').fill("200");
     await page.getByTestId('approxHoursFixedInput').fill("2");
-    await page.getByTestId("advancedWorkOrderTitle").fill('Work Order Title');
+    await page.waitForTimeout(2000);
+    await page.locator("#title").fill('Work Order Title');
+    await page.waitForTimeout(2000);
     //await page.locator('#financeId').click();
     //await page.getByRole('option', { name: '/Default/' }).click();
     //await page.keyboard.press('Enter');
     await page.getByRole('button', { name: "Publish", exact: true }).click();
+    await page.waitForTimeout(2000);
+
+    await page.getByRole('button', { name: "Report Problem" }).click();
+    await page.waitForTimeout(2000);
+    await page.locator(`xpath=//textarea[@data-testid="reportProblemExplanation"]`).fill("Explanation");
+    await page.locator(`xpath=//select[@data-testid="reportProblemDropDown"]`).click();
+    await page.getByRole('option', { name: 'How to/Technical assistance', exact: true }).click();
+    await page.getByRole('button', { name: "Report Problem", exact: true }).click();
 
 
     //Naviage to support case page
